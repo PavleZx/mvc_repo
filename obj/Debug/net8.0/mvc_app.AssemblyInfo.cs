49f5bf53a739317f7d0cef8f863fde4a8f2d8d43
PavleZx <pavlezulj01@gmail.com>
2026-03-09 12:56:11 +0100

    serialnumber name input added

--- obj/Debug/net8.0/mvc_app.AssemblyInfo.cs
+++ obj/Debug/net8.0/mvc_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f2cbfd5c9a756f44917a08ff8510c4dafe917b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed96c907e823bf4b569b7dc7f771f61266e50fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
